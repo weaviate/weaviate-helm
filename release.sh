6c92bcd446e63d5a1de33081a479c8cc5583be91
Etienne Dilocker <etienne@dilocker.de>
2021-01-12 18:07:10 +0100

    update to weaviate v1

--- release.sh
+++ release.sh
@@ -10,9 +10,6 @@ set -eou pipefail
   # cleanup in case there is a previous release already
   rm -rf "weaviate.tgz"
 
-  helm repo add bitnami https://charts.bitnami.com/bitnami
-  helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
-  helm repo add elastic https://helm.elastic.co
   helm dependencies build
   helm lint .
   helm package .
