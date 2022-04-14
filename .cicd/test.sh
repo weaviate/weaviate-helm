e10c42c397e9c1973206c4bf6328c969687f3d5a
Andrzej Liszka <andrzej.liszka@gmail.com>
2022-04-14 11:47:34 +0200

    WVT-63: Add support for passage and context endpoints in transformers module

--- .cicd/test.sh
+++ .cicd/test.sh
@@ -11,7 +11,36 @@ function check_modules() {
     echo "error: '$2' was not found"
     exit 1
   fi
-  rm -fr ./weaviate/out.yml
+  rm -fr ../weaviate/out.yml
+}
+
+function check_setting_has_value() {
+  local helm_settings=$1
+  local setting=$2
+  local expected_value=$3
+
+  echo "Test if '$setting' has value '$expected_value' using: '$helm_settings' settings"
+  helm template $helm_settings "weaviate.tgz" > out.yml
+  res=$(grep -F -C 1 "${setting}" < ../weaviate/out.yml)
+  if [[ $res != *$expected_value* ]]
+  then
+    echo "error: '$expected_value' was not found"
+    exit 1
+  fi
+  rm -fr ../weaviate/out.yml
+}
+
+function check_no_setting() {
+  local helm_settings=$1
+  local setting=$2
+
+  echo "Test if '$setting' is absent using: '$helm_settings' settings"
+  helm template $helm_settings "weaviate.tgz" > out.yml
+  if grep -Fq "$setting" ../weaviate/out.yml; then
+    echo "error: '$setting' was found"
+    exit 1
+  fi
+  rm -fr ../weaviate/out.yml
 }
 
 (
@@ -33,6 +62,7 @@ function check_modules() {
   check_modules "--set modules.text2vec-contextionary.enabled=false --set modules.qna-transformers.enabled=true" "value: qna-transformers"
   check_modules "--set modules.text2vec-contextionary.enabled=false --set modules.img2vec-neural.enabled=true" "value: img2vec-neural"
   check_modules "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.enabled=true" "value: text2vec-transformers"
+  check_modules "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.passageQueryServices.passage.enabled=true --set modules.text2vec-transformers.passageQueryServices.query.enabled=true" "value: text2vec-transformers"
   check_modules "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.enabled=true --set modules.img2vec-neural.enabled=true --set modules.qna-transformers.enabled=true" "value: text2vec-transformers,qna-transformers,img2vec-neural"
   check_modules "--set modules.img2vec-neural.enabled=true --set modules.qna-transformers.enabled=true" "value: text2vec-contextionary,qna-transformers,img2vec-neural"
   check_modules "--set modules.text2vec-contextionary.enabled=false --set modules.qna-transformers.enabled=true --set modules.img2vec-neural.enabled=true" "value: qna-transformers,img2vec-neural"
@@ -62,5 +92,23 @@ function check_modules() {
   check_modules "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.enabled=true --set modules.img2vec-neural.enabled=false --set modules.qna-transformers.enabled=true --set modules.text-spellcheck.enabled=true --set modules.ner-transformers.enabled=true --set modules.multi2vec-clip.enabled=true --set modules.text2vec-openai.enabled=true" "value: text2vec-transformers,qna-transformers,ner-transformers,text-spellcheck,multi2vec-clip,text2vec-openai"
   check_modules "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.enabled=false --set modules.img2vec-neural.enabled=false --set modules.qna-transformers.enabled=false --set modules.text-spellcheck.enabled=false --set modules.ner-transformers.enabled=false --set modules.multi2vec-clip.enabled=false --set modules.text2vec-openai.enabled=true" "value: text2vec-openai"
   check_modules "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.enabled=false --set modules.img2vec-neural.enabled=false --set modules.qna-transformers.enabled=false --set modules.text-spellcheck.enabled=false --set modules.ner-transformers.enabled=false --set modules.multi2vec-clip.enabled=false --set modules.text2vec-openai.enabled=true --set modules.text2vec-openai.apiKey=apiKey" "value: text2vec-openai"
+
+  check_setting_has_value "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.passageQueryServices.passage.enabled=true --set modules.text2vec-transformers.passageQueryServices.query.enabled=true" \
+    "name: TRANSFORMERS_PASSAGE_INFERENCE_API" \
+    "value: http://transformers-inference-passage.default.svc.cluster.local:8080"
+  check_setting_has_value "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.passageQueryServices.passage.enabled=true --set modules.text2vec-transformers.passageQueryServices.query.enabled=true" \
+    "name: TRANSFORMERS_QUERY_INFERENCE_API" \
+    "value: http://transformers-inference-query.default.svc.cluster.local:8080"
+  check_no_setting "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.passageQueryServices.passage.enabled=true --set modules.text2vec-transformers.passageQueryServices.query.enabled=true" \
+    "name: TRANSFORMERS_INFERENCE_API"
+    
+  check_setting_has_value "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.enabled=true" \
+    "name: TRANSFORMERS_INFERENCE_API" \
+    "value: http://transformers-inference.default.svc.cluster.local:8080"
+  check_no_setting "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.enabled=true" \
+    "name: TRANSFORMERS_PASSAGE_INFERENCE_API"
+  check_no_setting "--set modules.text2vec-contextionary.enabled=false --set modules.text2vec-transformers.enabled=true" \
+    "name: TRANSFORMERS_QUERY_INFERENCE_API"
+
   echo "Tests successful."
 )
