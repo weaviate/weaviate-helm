883445644abb0c36acdcbf59204a8c1066db81eb
Stefan Bogdan <stefanbogdan180394@gmail.com>
2022-08-09 11:37:00 +0200

    deploy helm to gh-pages

--- .cicd/release.sh
+++ .cicd/release.sh
@@ -15,4 +15,14 @@ set -eou pipefail
   helm package .
 
   mv "weaviate-$VERSION.tgz" "weaviate.tgz"
+
+  echo "Add chart version $VERSION to GitHub Pages"
+  cd ..
+  mkdir -p files-to-gh-pages
+  echo $(pwd)
+  helm package weaviate -d files-to-gh-pages
+  cp README.md files-to-gh-pages
+  cd files-to-gh-pages
+  helm repo index .
+  ls -ltr
 )
